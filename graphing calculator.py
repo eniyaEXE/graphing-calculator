88e6b2c36bd1a57b6d5c88a8b43c1e6187246ce9
Eniya <161463527+eniyaEXE@users.noreply.github.com>
2024-04-15 09:38:59 +0000

    graphing calculator v0.1.1
    - made it possible to just input "2x" instead of "2 * x" for example
    - added pi and e
    - made some small optimizations

--- graphing calculator.py
+++ graphing calculator.py
@@ -2,6 +2,28 @@
 import itertools
 import numpy as np
 import matplotlib.pyplot as plt
+import re
+import warnings
+
+def add_multiplication_operator(text):
+    pattern = r'(\d+)([a-zA-Z]+)'
+    result = re.sub(pattern, r'\1 * \2', text)
+    return result
+
+replacementDict = {
+    "Sin": "np.sin",
+    "Cos": "np.cos",
+    "Tan": "np.tan",
+    "arcsin": "np.arcsin",
+    "arccos": "np.arccos",
+    "arctan": "np.arctan",
+    "^": " ** ",
+    "sqrt": "np.sqrt",
+    "pi": "np.pi",
+    "e": "np.e"
+}
+
+warnings.filterwarnings("ignore", message="invalid value encountered in ") # ignores warnings about invalid values (for example, a negative number in sqrt), so that they do not get displayed in the terminal when graphing an equation with sqrt, arcsin etc.
 
 for _ in itertools.count():
     equation = input("\n[input your equation here]\n")     # inputs
@@ -10,17 +32,13 @@ for _ in itertools.count():
     equationInput = equation      # this saves the original input for later to be displayed
 
     # Replaces parts of the equation, so that you don't have to type "np.sin" or "**" etc. but can instead use regular mathematical notation. The Sin etc. are capitalized to avoid interference with the arcsin etc. (and have to be inputted this way)
-    equation = equation.replace("Sin", "np.sin")
-    equation = equation.replace("Cos", "np.cos")
-    equation = equation.replace("Tan", "np.tan")
-    equation = equation.replace("arcsin", "np.arcsin")
-    equation = equation.replace("arccos", "np.arccos")
-    equation = equation.replace("arctan", "np.arctan")
-    equation = equation.replace("^", " ** ")
-    equation = equation.replace("sqrt", "np.sqrt")
+    for old, new in replacementDict.items():
+        equation = equation.replace(old, new)
+
+    equation = add_multiplication_operator(equation)
 
     x = np.linspace(-1000, 1000, 100000)    # generates an array of 100.000 evenly spaced values between -1000 and 1000, and assigns it to x
-    y = eval(equation)                       # evaluates the equation (with the replaced parts) for every value in the array
+    y = eval(equation)                      # evaluates the equation (with the replaced parts) for every value in the array
 
     # Makes the matplotlib window for displaying the graph
     plt.title(f"Plot for {equationInput}")
